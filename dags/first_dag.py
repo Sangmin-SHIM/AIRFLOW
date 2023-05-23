04a141be2e6ad67686adf6c4a8f5b7bb06871ba5
sangmin <tkdals4723@gmail.com>
2023-05-23 12:35:22 +0200

    BashOperators example

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,36 +1,35 @@
-from datetime import datetime as dt
-from datetime import timedelta
-from airflow.utils.dates import days_ago#The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG#importing the operators required
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator#these args will get passed to each operator
-#these can be overridden on a per-task basis during operator #initialization#notice the start_date is any date in the past to be able to run it #as soon as it's created
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
 default_args = {
-'owner' : 'airflow',
-'depends_on_past' : False,
-'start_date' : days_ago(2),
-'email' : ['example@123.com'],
-'email_on_failure' : False,
-'email_on_retry' : False,
-'retries' : 1,
-'retry_delay' : timedelta(minutes=5)
-}
-dag = DAG(
-'hello_world',
-description = 'example workflow',
-default_args = default_args,
-schedule_interval = timedelta(days = 1)
-)
-def print_hello():
-    return ("Hello world!")
-#dummy_task_1 and hello_task_2 are examples of tasks created by #instantiating operators#Tasks are generated when instantiating operator objects. An object #instantiated from an operator is called a constructor. The first #argument task_id acts as a unique identifier for the task.#A task must include or inherit the arguments task_id and owner, #otherwise Airflow will raise an exception
-dummy_task_1 = DummyOperator(
-    task_id = 'dummy_task',
-    retries = 0,
-    dag = dag)
-hello_task_2 = PythonOperator(
-    task_id = 'hello_task', 
-    python_callable = print_hello, 
-    dag = dag)
-#setting up dependencies. hello_task_2 will run after the successful #run of dummy_task_1
-dummy_task_1 >> hello_task_2
+    "owner": "sangminSHIM",
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+    }
+
+with DAG(
+    dag_id="first_dag_v3",
+    default_args=default_args,
+    description="This is our first DAG",
+    start_date=datetime(2023, 5, 23),
+    schedule_interval="@daily",
+) as dag:
+    task1 = BashOperator(
+        task_id="my_first_task",
+        bash_command="echo 'Hello World! This is my first DAG'",
+    )
+
+    task2 = BashOperator(
+        task_id="my_second_task",
+        bash_command="echo 'It will be running after task1. This is my second DAG'",
+    )
+
+    task3 = BashOperator(
+        task_id="my_third_task",
+        bash_command="echo 'It will be running after task1 at the same time as task2. This is my third DAG'",
+    )
+
+    task1.set_downstream(task2)
+    task1.set_downstream(task3)
