9cfbb52ef177c16eb9c6015d6951553abb17b54a
Sangmin SHIM <tkdals4723@gmail.com>
2024-04-14 23:27:38 +0200

    2012 generated !

--- dags/main_dag.py
+++ dags/main_dag.py
@@ -14,7 +14,18 @@ import folium
 from folium import plugins
 from branca.element import Template, MacroElement
 from coordinate import clean_df_coordinate
-from csv_process import generate_template,convert_txt_to_csv_2017_2022, clean_pd_df_election_2017_2022, change_col_names_2017_2022,prepare_for_fusion_with_coordinate_2017_2022,match_col_coordinate_with_election_2017_2022,process_fusioned_data_2017_2022
+from csv_process import (
+    generate_template,
+    convert_txt_to_csv_2017_2022, 
+    clean_pd_df_election_2017_2022, 
+    change_col_names_2017_2022,
+    prepare_for_fusion_with_coordinate_2017_2022,
+    match_col_coordinate_with_election,
+    process_fusioned_data_2017_2022, 
+    convert_txt_to_csv_2012,
+    get_headers_2012,
+    clean_pd_df_election_2012,
+    get_pivot_table_2012)
     
 folder=Variable.get("DATA_INPUT_FOLDER")
 csv_folder=Variable.get("CSV_FOLDER")
@@ -31,9 +42,8 @@ def generate_coordinate_data():
     
 def process_2017_2022_result():
     task_identifiers=["PR_17_T1","PR_17_T2","PR_22_T1","PR_22_T2"]
-    
-    df_coordinate=pd.read_excel(f'{folder}/coordinate/coordinate.xlsx')
-    df_coordinate=match_col_coordinate_with_election_2017_2022(df_coordinate)
+    read_coordinate=pd.read_excel(f'{folder}/coordinate/coordinate.xlsx')
+    df_coordinate=match_col_coordinate_with_election(read_coordinate)
     
     for identifier in task_identifiers:
         # ----------------------
@@ -106,7 +116,7 @@ def process_2017_2022_result():
                                              .add_to(map), axis=1)    
         
         # ----------------------
-        # Legend
+        # Legend, Template
         # ----------------------
         legend_label_list = [{'text': key, 'color': value} for key, value in dict_nom.items()]
         template = generate_template(identifier,legend_label_list)
@@ -119,6 +129,102 @@ def process_2017_2022_result():
         map.get_root().add_child(macro)
         map.save(f'html_output/{identifier}.html')
     
+def process_2012_result():
+    identifier="PR_12"
+    tour_list=[1,2]
+    read_coordinate=pd.read_excel(f'{folder}/coordinate/coordinate.xlsx')
+    df_coordinate=match_col_coordinate_with_election(read_coordinate)
+
+    # ----------------------
+    # Variables : txt_file, csv_file
+    # ----------------------
+    
+    txt_file=Variable.get(f'ORIGINAL_TXT_{identifier}')
+    csv_file=Variable.get(f'CSV_{identifier}')
+
+    # ----------------------
+    # Election data cleaning
+    # ----------------------
+    convert_txt_to_csv_2012(f'{folder}/{txt_file}', f'{csv_folder}/{csv_file}')
+    df_election=pd.read_csv(f'{csv_folder}/{csv_file}', delimiter=';',encoding='latin-1', engine='python', names=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+    df_election=get_headers_2012(df_election)
+    
+    df_election=clean_pd_df_election_2012(df_election)
+    df_election=clean_pd_df_election_2012(df_election)
+    
+    df_PIVOT=get_pivot_table_2012(df_election)
+    
+    # ----------------------
+    # Fusion : Election data with coordinate data
+    # ----------------------
+    df_INNER_JOIN = pd.merge(df_PIVOT,df_coordinate, on=['Code du b.vote','Code du département'])
+    
+    # TOUR 1, TOUR 2
+    for tour in tour_list:
+    # ----------------------
+    # Color for each candidate
+    # ----------------------
+        map = folium.Map(location=[46.91160617052484, 2.153649265809747]
+                    , zoom_start=5.5)
+
+        tour_string=''
+        if tour ==1:
+            tour_string='1er'
+        else:
+            tour_string='2ème'
+        
+        # ---------------
+        # Color
+        # ---------------
+        list_candidate=[]
+
+        list_candidate.append(df_INNER_JOIN[df_INNER_JOIN['Tour'] == tour]['Candidat le plus voté'].tolist())
+
+        # Contained 2nd Array Dimension in List -> Remove 2nd Array, and take the elements in list 
+        list_candidate = sum(list_candidate, [])
+
+        # Remove Duplicated elements
+        dict_nom = dict.fromkeys(list_candidate)
+        list_candidat = list(set(zip(list_candidate)))
+
+        # Color for Each Politic Party
+        for key, value in dict_nom.items():
+            dict_nom[key]="#%06x" % random.randint(0, 0xFFFFFF)
+                
+        # ---------------
+        # Legend, Template
+        # ---------------
+        legend_label_list = [{'text': key, 'color': value} for key, value in dict_nom.items()]
+        
+        title = f'{identifier} {tour_string} tour'
+        
+        template = generate_template(title,legend_label_list)
+        macro = MacroElement()  
+        macro._template = Template(template)
+        
+        # ---------------
+        # Map
+        # ---------------
+        df_INNER_JOIN[df_INNER_JOIN['Tour'] == tour].apply(lambda row: folium.Circle(location=[row.loc['latitude'], row.loc['longitude']],
+                                                    radius=100,
+                                                    popup=row.loc['Candidat le plus voté'],
+                                                    fill=True, 
+                                                    tooltip="""
+                                                            Candidat : <b>"""+ str(row['Candidat le plus voté']) +"""</b><br>
+                                                            % Voix/Exp : <b>"""+ str(row['% Voix/Exp_le_plus_voté']) +"""</b><br>
+
+                                                            """, 
+                                                    color=dict_nom[row.loc['Candidat le plus voté']], 
+                                                    fill_color=dict_nom[row['Candidat le plus voté']],
+                                                    opacity=0.8,
+                                                    fill_opacity=0.7)
+                                                    .add_to(map), axis=1)  
+        # ---------------
+        # Save HTML
+        # ---------------
+        map.get_root().add_child(macro)
+        map.save(f'html_output/{identifier}_T{tour}.html')            
+            
 dag = DAG(
     'france_president_election',
     default_args={'start_date': days_ago(1)},
@@ -142,4 +248,10 @@ process_2017_2022_result = PythonOperator(
     dag=dag
 )
 
-generate_coordinate_data_task>>process_2017_2022_result
+process_2012_result = PythonOperator(
+    task_id='process_2012_result',
+    python_callable=process_2012_result,
+    dag=dag
+)
+
+generate_coordinate_data_task>>[process_2017_2022_result, process_2012_result]
--- plugins/__pycache__/csv_process.py
+++ plugins/__pycache__/csv_process.py
@@ -1,125 +1,9 @@
 import pandas as pd
 import re
 
-def convert_txt_to_csv_2017_2022(txt_file_path, csv_file_path):
-    try:
-        with open(txt_file_path, 'r', encoding='latin-1') as file:
-            first_line = file.readline()
-            content = file.read()
-            headers = first_line.split(';')
-    except Exception as e:
-        print(f"Error: Unable to read the text file. {e}")
-        return
-
-    content = content.replace(',', '.')
-    
-    try:
-        with open(txt_file_path, 'r', encoding='latin-1') as file:
-            last_line = file.readlines()[-1]        
-    except Exception as e:
-        print(f"Error: Unable to read the text file. {e}")
-        return
-    
-    nb_contents = len(last_line.split(';'))
-    nb_headers = len(headers)
-    nb_lack_header_cols = nb_contents - nb_headers
-
-    headers += [str(num) for num in range(nb_lack_header_cols)]
-    
-    try:
-        with open(csv_file_path, 'w', newline='') as csvfile:
-            csvfile.write(';'.join(headers) + '\n')
-            csvfile.write(content)
-            
-            return headers
-    except Exception as e:
-        print(f"Error: Unable to write to the CSV file. {e}")
-        return
-    
-def clean_pd_df_election_2017_2022(df_election):
-    # Drop Nan data
-    df_election = df_election[df_election['Code du département'].notnull()]
-    df_election = df_election[df_election['Code de la commune'].notnull()]
-    df_election = df_election[df_election['Code du b.vote'].notnull()]
-
-    # Numeric column
-    df_election['Code du département'] = pd.to_numeric(df_election['Code du département'], errors='coerce')
-    df_election['Code de la commune'] = pd.to_numeric(df_election['Code de la commune'], errors='coerce')
-    df_election['Code du b.vote'] = pd.to_numeric(df_election['Code du b.vote'], errors='coerce')
-    
-    return df_election
-
-def change_col_names_2017_2022(df_election):
-    # Define the pattern to match column names ending with '0', '1', '2', etc.
-    pattern = r'\d+$'  # Matches names ending with underscore followed by digits
-
-    # Find columns that match the pattern
-    matching_columns = [col for col in df_election.columns if re.match(pattern, col)]
-
-    # Define the replacement strings
-    replacement_strings=['N°Panneau', 'Sexe', 'Nom', 'Prénom', '% Voix', '% Voix/Ins', '% Voix/Exp']
-
-    # Define the number of columns to iterate over
-    num_replacement_cols = len(replacement_strings)
-    num_matching_cols = len(matching_columns)
-
-    # Replace the underscores in the column names iterably with the replacement strings
-    for i in range(num_matching_cols):
-        col_index = (i // num_replacement_cols) + 1
-        replacement_index = (i % num_replacement_cols) + 1
-        replacement = replacement_strings[replacement_index - 1]
-        new_col_name = f"{replacement}_{col_index}"
-        df_election.rename(columns={matching_columns[i]: new_col_name}, inplace=True)
-    return df_election
-
-def prepare_for_fusion_with_coordinate_2017_2022(df_election):
-    # Multiply 1000
-    df_election['Code du département'] = (df_election['Code du département'].apply(lambda x: x * 1000)).astype(int)
-
-    # Add Code du département + Code de la commune
-    df_election['Code du département'] = (df_election['Code du département'] + df_election['Code de la commune']).astype(int)
-    
-    return df_election
-
-def match_col_coordinate_with_election_2017_2022(df_coordinate):
-    # Matching the column name for fusioning with Election présidentielle
-    df_coordinate.rename(columns={'code':'Code du b.vote'}, inplace=True)
-    df_coordinate.rename(columns={'commune_code':'Code du département'}, inplace=True)
-    return df_coordinate
-
-def process_fusioned_data_2017_2022(df_INNER_JOIN):
-    # Matches names ending with underscore followed by digits
-    pattern = r"% Voix/Exp(?:_\d+)?"  
-
-    columns_voix_exp_list=[col for col in df_INNER_JOIN.columns if re.match(pattern, col)]
-    df_voix_exp=df_INNER_JOIN[columns_voix_exp_list]
-    df_voix_exp = df_voix_exp.astype('float64')
-    
-    nb_candidates=len(columns_voix_exp_list)
-    candidates=[]
-    
-    for num in range(nb_candidates):
-        if (num ==0):
-            candidates.append({'nom': df_INNER_JOIN["Nom"][0], 'prenom':df_INNER_JOIN["Prénom"][0]})
-            continue
-        candidates.append({'nom':df_INNER_JOIN[f"Nom_{num}"][0], 'prenom':df_INNER_JOIN[f"Prénom_{num}"][0]})    
-
-
-    df_INNER_JOIN['NOM_candidat_le_plus_voté']=df_voix_exp.idxmax(axis=1).str.replace('% Voix/Exp','Nom')
-    df_INNER_JOIN['PRENOM_candidat_le_plus_voté']=df_voix_exp.idxmax(axis=1).str.replace('% Voix/Exp','Prénom')
-
-    df_INNER_JOIN['% Voix/Exp_le_plus_voté']=df_voix_exp.max(axis=1)  
-
-    for num in range(nb_candidates):
-        if (num ==0):
-            df_INNER_JOIN.loc[(df_INNER_JOIN['NOM_candidat_le_plus_voté'] =='Nom') | (df_INNER_JOIN['NOM_candidat_le_plus_voté'] =='Nom\n'), 'NOM_candidat_le_plus_voté'] = candidates[0]['nom']
-            df_INNER_JOIN.loc[(df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] =='Prénom') | (df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] =='Prénom\n'), 'PRENOM_candidat_le_plus_voté'] = candidates[0]['prenom']
-            continue
-        df_INNER_JOIN.loc[(df_INNER_JOIN['NOM_candidat_le_plus_voté'] ==f'Nom_{num}'),'NOM_candidat_le_plus_voté'] = candidates[num]['nom']
-        df_INNER_JOIN.loc[(df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] ==f'Prénom_{num}'),'PRENOM_candidat_le_plus_voté'] = candidates[num]['prenom']    
-    
-    return df_INNER_JOIN
-
+# --------------------------------
+# Global functions
+# --------------------------------
 def generate_template(title, legend_label_list):
     legend_labels_html = ""
     for label in legend_label_list:
@@ -212,3 +96,196 @@ def generate_template(title, legend_label_list):
     {% endmacro %}"""
     
     return template
+
+def match_col_coordinate_with_election(df_coordinate):
+    # Matching the column name for fusioning with Election présidentielle
+    df_coordinate.rename(columns={'code':'Code du b.vote'}, inplace=True)
+    df_coordinate.rename(columns={'commune_code':'Code du département'}, inplace=True)
+    return df_coordinate
+
+# --------------------------------
+# 2017, 2022 Election data cleaning
+# --------------------------------
+def convert_txt_to_csv_2017_2022(txt_file_path, csv_file_path):
+    try:
+        with open(txt_file_path, 'r', encoding='latin-1') as file:
+            first_line = file.readline()
+            content = file.read()
+            headers = first_line.split(';')
+    except Exception as e:
+        print(f"Error: Unable to read the text file. {e}")
+        return
+
+    content = content.replace(',', '.')
+    
+    try:
+        with open(txt_file_path, 'r', encoding='latin-1') as file:
+            last_line = file.readlines()[-1]        
+    except Exception as e:
+        print(f"Error: Unable to read the text file. {e}")
+        return
+    
+    nb_contents = len(last_line.split(';'))
+    nb_headers = len(headers)
+    nb_lack_header_cols = nb_contents - nb_headers
+
+    headers += [str(num) for num in range(nb_lack_header_cols)]
+    
+    try:
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csvfile.write(';'.join(headers) + '\n')
+            csvfile.write(content)
+            
+            return headers
+    except Exception as e:
+        print(f"Error: Unable to write to the CSV file. {e}")
+        return
+    
+def clean_pd_df_election_2017_2022(df_election):
+    # Drop Nan data
+    df_election = df_election[df_election['Code du département'].notnull()]
+    df_election = df_election[df_election['Code de la commune'].notnull()]
+    df_election = df_election[df_election['Code du b.vote'].notnull()]
+
+    # Numeric column
+    df_election['Code du département'] = pd.to_numeric(df_election['Code du département'], errors='coerce')
+    df_election['Code de la commune'] = pd.to_numeric(df_election['Code de la commune'], errors='coerce')
+    df_election['Code du b.vote'] = pd.to_numeric(df_election['Code du b.vote'], errors='coerce')
+    
+    return df_election
+
+def change_col_names_2017_2022(df_election):
+    # Define the pattern to match column names ending with '0', '1', '2', etc.
+    pattern = r'\d+$'  # Matches names ending with underscore followed by digits
+
+    # Find columns that match the pattern
+    matching_columns = [col for col in df_election.columns if re.match(pattern, col)]
+
+    # Define the replacement strings
+    replacement_strings=['N°Panneau', 'Sexe', 'Nom', 'Prénom', '% Voix', '% Voix/Ins', '% Voix/Exp']
+
+    # Define the number of columns to iterate over
+    num_replacement_cols = len(replacement_strings)
+    num_matching_cols = len(matching_columns)
+
+    # Replace the underscores in the column names iterably with the replacement strings
+    for i in range(num_matching_cols):
+        col_index = (i // num_replacement_cols) + 1
+        replacement_index = (i % num_replacement_cols) + 1
+        replacement = replacement_strings[replacement_index - 1]
+        new_col_name = f"{replacement}_{col_index}"
+        df_election.rename(columns={matching_columns[i]: new_col_name}, inplace=True)
+    return df_election
+
+def prepare_for_fusion_with_coordinate_2017_2022(df_election):
+    # Multiply 1000
+    df_election['Code du département'] = (df_election['Code du département'].apply(lambda x: x * 1000)).astype(int)
+
+    # Add Code du département + Code de la commune
+    df_election['Code du département'] = (df_election['Code du département'] + df_election['Code de la commune']).astype(int)
+    
+    return df_election
+
+def process_fusioned_data_2017_2022(df_INNER_JOIN):
+    # Matches names ending with underscore followed by digits
+    pattern = r"% Voix/Exp(?:_\d+)?"  
+
+    columns_voix_exp_list=[col for col in df_INNER_JOIN.columns if re.match(pattern, col)]
+    df_voix_exp=df_INNER_JOIN[columns_voix_exp_list]
+    df_voix_exp = df_voix_exp.astype('float64')
+    
+    nb_candidates=len(columns_voix_exp_list)
+    candidates=[]
+    
+    for num in range(nb_candidates):
+        if (num ==0):
+            candidates.append({'nom': df_INNER_JOIN["Nom"][0], 'prenom':df_INNER_JOIN["Prénom"][0]})
+            continue
+        candidates.append({'nom':df_INNER_JOIN[f"Nom_{num}"][0], 'prenom':df_INNER_JOIN[f"Prénom_{num}"][0]})    
+
+
+    df_INNER_JOIN['NOM_candidat_le_plus_voté']=df_voix_exp.idxmax(axis=1).str.replace('% Voix/Exp','Nom')
+    df_INNER_JOIN['PRENOM_candidat_le_plus_voté']=df_voix_exp.idxmax(axis=1).str.replace('% Voix/Exp','Prénom')
+
+    df_INNER_JOIN['% Voix/Exp_le_plus_voté']=df_voix_exp.max(axis=1)  
+
+    for num in range(nb_candidates):
+        if (num ==0):
+            df_INNER_JOIN.loc[(df_INNER_JOIN['NOM_candidat_le_plus_voté'] =='Nom') | (df_INNER_JOIN['NOM_candidat_le_plus_voté'] =='Nom\n'), 'NOM_candidat_le_plus_voté'] = candidates[0]['nom']
+            df_INNER_JOIN.loc[(df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] =='Prénom') | (df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] =='Prénom\n'), 'PRENOM_candidat_le_plus_voté'] = candidates[0]['prenom']
+            continue
+        df_INNER_JOIN.loc[(df_INNER_JOIN['NOM_candidat_le_plus_voté'] ==f'Nom_{num}'),'NOM_candidat_le_plus_voté'] = candidates[num]['nom']
+        df_INNER_JOIN.loc[(df_INNER_JOIN['PRENOM_candidat_le_plus_voté'] ==f'Prénom_{num}'),'PRENOM_candidat_le_plus_voté'] = candidates[num]['prenom']    
+    
+    return df_INNER_JOIN
+
+# --------------------------------
+# 2002, 2007 2012 Election data cleaning
+# --------------------------------
+def convert_txt_to_csv_2012(txt_file_path, csv_file_path):
+    try:
+        with open(txt_file_path, 'r', encoding='latin-1') as file:
+            # Read all lines into a list
+            lines = file.readlines()
+
+            # Filter out lines containing '--'
+            filtered_lines = [line for line in lines if '--' not in line]
+
+            # Join the filtered lines back into a single string
+            content = ''.join(filtered_lines)            
+    except Exception as e:
+        print(f"Error: Unable to read the text file. {e}")
+        return
+
+    try:
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csvfile.write(content)    
+            return            
+    except Exception as e:
+        print(f"Error: Unable to write to the CSV file. {e}")
+        return
+    
+def get_headers_2012(df_election):
+    # Delete useless columns
+    df_election=df_election.drop(columns=[3,4,5,13])
+
+    # Set the headers according to the information of raw data
+    headers=['Tour','Code du département', 'Code de la commune', 'Code du b.vote', 'Inscrits', 'Votants', 'Exprimés', 'No. de dépôt du candidat','Nom du candidat', 'Prénom du candidat','Nombre de voix du candidat']
+    df_election.columns = headers
+    
+    return df_election
+
+def clean_pd_df_election_2012(df_election):    
+    # Drop Nan data
+    df_election = df_election[df_election['Code du département'].notnull()]
+    df_election = df_election[df_election['Code de la commune'].notnull()]
+    df_election = df_election[df_election['Code du b.vote'].notnull()]
+
+    # Numeric column
+    df_election['Code du département'] = pd.to_numeric(df_election['Code du département'], errors='coerce')
+    df_election['Code de la commune'] = pd.to_numeric(df_election['Code de la commune'], errors='coerce')
+    df_election['Code du b.vote'] = pd.to_numeric(df_election['Code du b.vote'], errors='coerce')
+    
+    return df_election
+
+def get_pivot_table_2012(df_election):
+    common_columns = ['Tour','Code du département', 'Code de la commune', 'Code du b.vote', 'Inscrits', 'Votants', 'Exprimés']
+
+    df_election['Nom complet du candidat'] = df_election['Nom du candidat'].astype(str) + ' ' + df_election['Prénom du candidat'].astype(str)
+
+    df_PIVOT = df_election.pivot_table(index=common_columns, columns='Nom complet du candidat', values='Nombre de voix du candidat', aggfunc=lambda x: x)
+    df_PIVOT.reset_index(inplace=True)
+ 
+    df_PIVOT['Candidat le plus voté'] = df_PIVOT.drop(common_columns, axis=1).idxmax(axis=1)
+    df_PIVOT['Vote le plus voté'] = df_PIVOT.apply(lambda row: row[row['Candidat le plus voté']], axis=1)
+        
+    # Multiply 1000
+    df_PIVOT['Code du département'] = (df_PIVOT['Code du département'].apply(lambda x: x * 1000)).astype(int)
+
+    # Add Code du département + Code de la commune
+    df_PIVOT['Code du département'] = (df_PIVOT['Code du département'] + df_PIVOT['Code de la commune']).astype(int)
+
+    # Calculate % Voix/Exp_le_plus_voté : 
+    df_PIVOT['% Voix/Exp_le_plus_voté'] = round((df_PIVOT['Vote le plus voté']/df_PIVOT['Exprimés'])*100,2)    
+    
+    return df_PIVOT
